10b039a3c89da9c1cf625dbe02e781d4a4edd107
StaVergos <stavrospvergos@gmail.com>
2025-06-09 00:30:28 +0300

    Added load data endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from data_loader import stream_airline_csv_to_minio
 
 app = FastAPI()
 
@@ -6,3 +7,12 @@ app = FastAPI()
 @app.get("/")
 def root():
     return "Hey"
+
+
+@app.get("/load-data")
+def load_data():
+    try:
+        filename = stream_airline_csv_to_minio()
+        return {"message": f"Data loaded successfully: {filename}"}
+    except Exception as e:
+        return {"error": str(e)}
