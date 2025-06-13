af0043f705ad26b71654cd0179a508b16e5f4993
StaVergos <stavrospvergos@gmail.com>
2025-06-13 20:34:58 +0300

    Changed dataset

--- backend/src/config.py
+++ backend/src/config.py
@@ -11,7 +11,7 @@ logging.basicConfig(
 
 KAGGLE_USERNAME = os.getenv("KAGGLE_USERNAME")
 KAGGLE_KEY = os.getenv("KAGGLE_KEY")
-FILENAME = "Airline_Market_Fare_Prediction_Data/MarketFarePredictionData.csv"
+FILENAME = "US Airline Flight Routes and Fares 1993-2024.csv"
 MODEL_FILENAME = "random_forest_regressor_model.pkl"
 MINIO_ENDPOINT = "http://minio:9000"
 ACCESS_KEY = "minioadmin"
@@ -19,8 +19,11 @@ SECRET_KEY = "minioadmin"
 BUCKET_NAME = "airplane-tickets"
 REGION_NAME = "us-east-1"
 SIGNATURE_VERSION = "s3v4"
-DATASET_URL = "https://www.kaggle.com/api/v1/datasets/download/orvile/airline-market-fare-prediction-data"
-
+DATASET_URL = (
+    "https://www.kaggle.com/api/v1/datasets/download/"
+    "bhavikjikadara/us-airline-flight-routes-and-fares-1993-2024"
+    "?datasetVersionNumber=1"
+)
 
 logging.info(
     f"Configuration loaded successfully with {'KAGGLE_USERNAME' if KAGGLE_USERNAME else 'KAGGLE_KEY' if KAGGLE_KEY else 'no credentials'}"
